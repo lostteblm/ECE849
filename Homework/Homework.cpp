ed1c0d5035d0c0b5451f105eea84181f0e10300b
desktop <lostteblm@gmail.com>
2014-09-14 16:45:12 -0400

    HW2

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -1,5 +1,5 @@
-// Homework.cpp : Defines the entry point for the console application.
-//
+// Heyu Liu- C15622730
+//Assignment-1
 
 #include <afxwin.h>  // necessary for MFC to work properly
 #include <iostream>
@@ -15,6 +15,7 @@ using namespace blepo;
 using namespace std;
 
 
+//Square the image function
 void SquareRegion(const ImgBgr& Img1,ImgGray& Img2)
 {
 	int width = Img1.Width();
@@ -22,8 +23,8 @@ void SquareRegion(const ImgBgr& Img1,ImgGray& Img2)
 	Img2.Reset(width, height);
 	
 
-	//Modify image that are larger than 100*100
-	if (width > 100 && height > 100)
+	//Modify images that are larger than 100*100
+	if (width >= 100 && height >= 100)
 	{
 		for (ImgGray::Iterator p = Img2.Begin(); p != Img2.End(); p++)
 		{
@@ -37,7 +38,7 @@ void SquareRegion(const ImgBgr& Img1,ImgGray& Img2)
 			}
 		}
 	}
-	//modify images that are small than 100*100
+	//modify images that are smaller than 100*100
 	else
 	{
 		for (ImgGray::Iterator p = Img2.Begin(); p != Img2.End(); p++)
@@ -47,6 +48,8 @@ void SquareRegion(const ImgBgr& Img1,ImgGray& Img2)
 	}
 }
 
+
+//Mask image function
 ImgBgr maskimage(const ImgGray& source,ImgBgr target)
 {
 	int count = 0;
@@ -65,6 +68,8 @@ ImgBgr maskimage(const ImgGray& source,ImgBgr target)
 	return target;
 }
 
+
+//Main function
 int main(int argc, const char* argv[], const char* envp[])
 {
     // Initialize MFC and return if failure
@@ -75,44 +80,22 @@ int main(int argc, const char* argv[], const char* envp[])
         return 1;
     }
 
-
 	try
 	{
-		//Initialize filenames
+		//Initialization filenames
 		string location = "../../images/";
-		ImgBgr img1(0, 0);
-		ImgGray img2(0, 0);
-		ImgGray img3(0, 0);
-
+		ImgBgr img(0, 0);
 
 		//check if three file names are correctly inputted
-		if (4 == argc)
+		if (2 == argc)
 		{
 			//Load three file names
-			string filename1 = argv[1];
-			string filename2 = argv[2];
-			string filename3 = argv[3];
-			cout << "Three files are: " << argv[1] << ", " << argv[2] << ", " << argv[3] << endl;
+			string filename = argv[1];
+			cout << "Three files are: " << argv[1] << endl;
 
-			//check if the second file has a jpg extension
-			while (true)
-			{
-				int startp = filename2.find(".");
-				string extension2 = filename2.substr(startp+1);
-				if ("jpeg" == extension2 || "jpg" == extension2 || "JPG" == extension2 || "JPEG" == extension2)
-				{
-					cout << "ERROR: The extension of filename2 cannot be JPEG, please type another extension" << endl;
-				}
-				else
-				{
-					break;
-				}
-				getline(cin, filename2);
-			}
 
 			//Load first two file locations
-			string location1 = location + filename1;
-			string location2 = location + filename2;
+			string location1 = location + filename;
 
 			//check if the first file exist
 			while (true)
@@ -120,74 +103,18 @@ int main(int argc, const char* argv[], const char* envp[])
 				if ((_access(location1.c_str(), 0)) != -1)
 				{
 					//Load the first image
-					Load(location1.c_str(), &img1);
+					Load(location1.c_str(), &img);
+				}
+				if (img.Height() > 0 && img.Width() > 0)
+				{
 					break;
 				}
-				cout << "ERROR: filename1 doesn't exist, please open another file" << endl;
-				filename1 = "";
-				getline(cin, filename1);
-				location1 = location + filename1;
+				cout << "ERROR: cannot open " << filename << ", please open another file" << endl;
+				filename = "";
+				getline(cin, filename);
+				location1 = location + filename;
 			}
-
-			//modify the first image and save the second image
-			SquareRegion(img1, img2);
-			Figure fig1, fig2;
-			fig1.Draw(img1);
-			fig2.Draw(img2);
-			Save(img2, location2.c_str(), "bmp");
-
-			//check if the second file is successfully saved
-			if ((_access(location2.c_str(), 0)) != -1)
-			{
-				
-				//check if the third file exist
-				while (true)
-				{
-					string location3 = location + filename3;
-					if ((_access(location3.c_str(), 0)) != -1)
-					{
-						Load(location3.c_str(), &img3);
-						break;
-					}
-					else
-					{
-						cout << "ERROR: filename3 doesn't exist, please open another file" << endl;
-						filename3 = "";
-						getline(cin, filename3);
-						location3 = location + filename3;
-					}
-				}
-
-				//mask the third image
-				while (true)
-				{ 
-					//check if the third image has same size with the first image
-					if (img3.Width() == img1.Width() && img3.Height() == img1.Height())
-					{
-						ImgBgr maskedfigure = maskimage(img3, img1);
-						Figure fig3;
-						fig3.Draw(img3);
-						Figure fig4;
-						fig4.Draw(maskedfigure);
-						break;
-					}
-					else
-					{
-						cout << "The sizes of filename3 and filename1 are different,please select another file" << endl;
-						filename3 = "";
-						getline(cin, filename3);
-						string location3 = location + filename3;
-					}
-				}
-
-			}
-				else
-				{
-					//Second file failed to save
-					cout << "ERROR: location2 failed to save" << endl;
-				}
-
-			}
+		}
 		else
 		{
 			//Failed to input three file names
